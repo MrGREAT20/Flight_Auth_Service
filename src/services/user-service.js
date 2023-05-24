a92aac5e1f7614f41b60fe685b7cc5148df53940
MrGREAT20 <pralaywandre20@gmail.com>
2023-05-24 17:51:25 +0530

    Added function to compare user inputted password and encrypted password using bcrypt

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,7 @@
 const UserRepository = require('../repository/user-repository');
-
+const jwt = require('jsonwebtoken');
+const {JWT_KEY} = require('../config/serverConfig');
+const bcrypt = require('bcrypt');
 class UserService{
     constructor(){
         this.userRepository = new UserRepository();
@@ -10,6 +12,43 @@ class UserService{
             return user;
         } catch (error) {
             console.log("Something went wrong at service layer");
+            throw error;
+        }
+    }
+    createToken(user){
+        /**
+         * this function will generate the JWT token with the help of user object
+         * 
+         */
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
+            return result;
+            
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+    verifyToken(token){
+        /**
+         * this function will verify the token from the client side
+         */
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validification");
+            throw error;
+        }
+    }
+    checkpassword(userinputplainpassword, encryptedpassword){
+        try {
+            return bcrypt.compareSync(userinputplainpassword, encryptedpassword);
+            /**
+             * to check whether the userinputed password matches the encrypted password
+             */
+        } catch (error) {
+            console.log("Something went wrong in password verification");
             throw error;
         }
     }
