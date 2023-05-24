b2056d44be2e27986d19db8b47e716203fbc3023
MrGREAT20 <pralaywandre20@gmail.com>
2023-05-24 19:12:31 +0530

    added SignIn Function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,9 +2,6 @@ const UserRepository = require('../repository/user-repository');
 const jwt = require('jsonwebtoken');
 const {JWT_KEY} = require('../config/serverConfig');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const {JWT_KEY} = require('../config/serverConfig');
-const bcrypt = require('bcrypt');
 class UserService{
     constructor(){
         this.userRepository = new UserRepository();
@@ -15,6 +12,61 @@ class UserService{
             return user;
         } catch (error) {
             console.log("Something went wrong at service layer");
+            throw error;
+        }
+    }
+    createToken(user){
+        /**
+         * this function will generate the JWT token with the help of user object
+         * 
+         */
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
+            return result;
+            
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+    verifyToken(token){
+        /**
+         * this function will verify the token from the client side
+         */
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validification");
+            throw error;
+        }
+    }
+    checkpassword(userinputplainpassword, encryptedpassword){
+        try {
+            return bcrypt.compareSync(userinputplainpassword, encryptedpassword);
+            /**
+             * to check whether the userinputed password matches the encrypted password
+             */
+        } catch (error) {
+            console.log("Something went wrong in password verification");
+            throw error;
+        }
+    }
+    async signIn(email, plainpassword){
+        try {
+            //step1: fetch the user using email
+            const user = await this.userRepository.getByEmail(email);
+            //step2: compare the plainpassword with the encrypted stored password
+            const passwordcheck = this.checkpassword(plainpassword, user.password);
+            if(!passwordcheck){
+                console.log("Password doesnt match");
+                throw{error:'Incorrect Password'};
+            }
+            //step3: if password matches, create a token and send it to the user
+            const newJwt = this.createToken({email:email, id:user.id});
+            return newJwt;
+        } catch (error) {
+            console.log("Something went wrong in SignIn process at user-service layer");
             throw error;
         }
     }
